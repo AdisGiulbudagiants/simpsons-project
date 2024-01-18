0eae0aa52abccbfa8f9961b951029c81fce30767
Adis Giulbudagiants <adisforwork2001@gmail.com>
2024-01-18 21:11:56 +0400

    NotFound page is done

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,11 @@
 const NotFound = () => {
-  return <div>NotFound</div>
+  return (
+    <div className="320px-639px:items-start 320px-639px:h-[40%]  w-[100vw] h-[80%] flex justify-center items-center">
+      <h1 className="320px-639px:text-[6rem] sm:text-[9rem] md:text-[13rem] text-center font-caveat font-bold drop-shadow-lg text-yellow">
+        Page Not Found
+      </h1>
+    </div>
+  )
 }
 
 export default NotFound
