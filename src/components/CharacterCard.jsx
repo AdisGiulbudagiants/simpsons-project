26772195867b6d8f8932a48685894e81bd9f795c
Adis Giulbudagiants <adisforwork2001@gmail.com>
2024-01-25 15:16:48 +0700

    Fix CharacterCard in small resolution

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -2,14 +2,14 @@ const CharacterCard = ({ id, quote, character, image }) => {
   return (
     <div
       key={id}
-      className="320px-639px:w-[80%] sm:w-[80%] xl:w-[62.5rem] mx-auto 320px-639px:p-1 sm:p-3 mb-5 bg-white border rounded-md"
+      className="320px-639px:w-[90%] sm:w-[80%] xl:w-[62.5rem] mx-auto 320px-639px:p-1 sm:p-3 mb-5 bg-white border rounded-md"
     >
       <img className="mb-2 mx-auto" src={image} alt="character" />
       <div className="flex flex-col items-center p-2">
-        <p className="text-[1.4rem] bg-pink mb-[1rem] mx-5 p-3 border border-black rounded-md text-white font-poppins text-center">
+        <p className=" 320px-639px:text-[1rem] text-[1.4rem] bg-pink mb-[1rem] mx-5 p-3 border border-black rounded-md text-white font-poppins text-center">
           {quote}
         </p>
-        <p className="text-[1.8rem] bg-pink mx-5 p-3 border border-black rounded-md text-white font-poppins text-center">
+        <p className=" 320px-639px:text-[1.2rem] text-[1.8rem] bg-pink mx-5 p-3 border border-black rounded-md text-white font-poppins text-center">
           {character}
         </p>
       </div>
